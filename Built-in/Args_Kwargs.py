64903c4c4d035369a1066ef116095ccd4a45dfb8
Bruno Dias Leite <brunodleite2@gmail.com>
2020-02-20 17:35:26 -0800

    Basic concepts

---
+++ Built-in/Args_Kwargs.py
@@ -0,0 +1,24 @@
+"""
+*args : when you are not sure how many args
+will be passed to a function
+"""
+
+def fargs(arg1, *args):
+    print(arg1)
+    print(args)
+    print(type(args))
+
+fargs(1, 2, 3, 4)
+fargs(1)
+
+"""
+**kwargs : when you are not sure how many 
+keyworded args will be passed to a function
+"""
+
+def fkwargs(arg1, **k):
+    print(arg1)
+    print(k)
+    print(type(k))
+
+fkwargs(1, two=2, three=3, four=4)
--- playground.py
+++ playground.py
@@ -1,1 +1,10 @@
-print("Welcome")
+"""
+FUNCTIONS
+"""
+
+def f(a, b, c):
+    print('%s %s %s' % (a, b, c))
+
+
+f('a', 'c', 'b')
+f(a='a', c='c', b='b')
