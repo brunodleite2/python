8b9855cebb1a6a741e7a0c095cf09778cf7b9e2a
Bruno Dias Leite <brunodleite2@gmail.com>
2020-02-27 11:21:29 -0800

    LC-08 second solution

--- LC/08-tring-to-integer-atoi.py
+++ LC/08-tring-to-integer-atoi.py
@@ -1,3 +1,5 @@
+'FIRST SOLUTION - NO BULTIN FUCTIONS USED - FASTER'
+
 class Solution:
     MAX = 2**31 -1
     MIN = 2**31 * -1
@@ -58,6 +60,60 @@ class Solution:
 
         return self.__convert_to_int(response)
 
+'SECOND SOLUTION - STR BULTIN FUCTIONS USED - SLOWER'
+
+class Solution:
+    MAX = 2**31 -1
+    MIN = 2**31 * -1
+
+    def __is_sign(self, char: str) -> bool:
+        if char in ['-','+']:
+            return True
+        return False
+
+    def __convert_to_int(self, str: str) -> int:
+        str_len =  len(str)
+        if not str_len or (str_len == 1 and not str[0].isnumeric()):
+            return 0
+
+        integer = int(str)
+
+        if integer > Solution.MAX:
+            return Solution.MAX
+
+        if integer < Solution.MIN:
+            return Solution.MIN
+
+        return integer
+
+
+    def myAtoi(self, str: str) -> int:
+        number_has_started = False
+        response = ""
+
+        #str = str.lstrip()
+        for char in str:
+            if not number_has_started:
+
+                if char.isspace():
+                    continue
+
+                if self.__is_sign(char) or char.isnumeric():
+                    response += char
+                    number_has_started = True
+                    continue
+                # Letter!!
+                return 0
+
+            # number has started
+            if char.isnumeric():
+                response += char
+                continue
+
+            # number has finished
+            return self.__convert_to_int(response)
+
+        return self.__convert_to_int(response)
 
 solution = Solution()
 assert solution.myAtoi('42') == 42
